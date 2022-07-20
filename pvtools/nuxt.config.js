0877b2dc2809b916d8f901bf4a800ede9428c863
Nick81nrw <nick@mybergmann.net>
2022-07-20 13:56:49 +0200

    cleanup

--- pvtools/nuxt.config.js
+++ pvtools/nuxt.config.js
@@ -45,7 +45,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-     baseURL: /*'https://pvtools.sektorsonne.de'*/ 'http://localhost:8082',
+     baseURL: 'https://pvtools.sektorsonne.de',
+    //  baseURL: 'http://localhost:8082',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
