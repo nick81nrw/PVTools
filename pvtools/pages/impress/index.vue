0ab5cc698c5847206f523cf4caf8b1dbd9f6372e
Nick81nrw <nick@mybergmann.net>
2022-07-20 13:56:31 +0200

    new page impress

---
+++ pvtools/pages/impress/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col>
+                <h1>Impressum</h1>
+
+                <p>Andreas Schmitz</p>
+                <p>Einzelunternehmen</p>
+                <p>c/o Grosch Postflex #1662</p>
+                <p>Emsdettener Str. 10</p>
+                <p>48268 Greven</p>
+                <p>Keine Pakete oder Päckchen - Annahme wird verweigert!</p>
+                <p>akkudoktor18650@gmail.com</p>
+                <p>+4915782144960</p>
+            </b-col>
+            
+        </b-row>
+        <b-row>
+            <b-col>
+                <h2>Disclaimer:</h2>
+                <p>Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor, welche sich auf Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen.</p>
+                <p>Die kostenlosen und frei zugänglichen Inhalte dieser Webseite und die des YouTube Kanals wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Webseite und die des YouTube Kanals übernimmt jedoch keine Gewähr für die Richtigkeit und Aktualität der bereitgestellten kostenlosen und frei zugänglichen journalistischen Ratgeber und Nachrichten. Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors und nicht immer die Meinung des Anbieters wieder. Allein durch den Aufruf der kostenlosen und frei zugänglichen Inhalte kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande, insoweit fehlt es am Rechtsbindungswillen des Anbieters.</p>
+                <p>Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.
+Der Umgang mit Akkus, Batterien und der Elektronik ist gefährlich und ich übernehme keine Haftung für Schäden oder Fehler jeglicher Art. Diese Videos dokumentieren nur mein persönliches Projekt.
+Wenn du so etwas nachbauen möchtest, reichen meine Videos als Grundlage nicht aus. Du brauchst viel mehr Informationen, Erfahrung und eine Menge Planung.</p>
+            </b-col>
+        </b-row>
+        <NuxtLink to="/">Home</NuxtLink>
+    </b-container>
+
+</template>
+<style>
+    p {
+        margin: 5px 0;
+    }
+    h2 {
+        margin-top: 20px;
+    }
+</style>
