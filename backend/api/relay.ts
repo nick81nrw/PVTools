3313a1d4a842261f7dac92ead5f9251d68af4112
Nick81nrw <nick@mybergmann.net>
2022-08-01 14:02:16 +0200

    fix url check for openstreetmap

--- backend/api/relay.ts
+++ backend/api/relay.ts
@@ -2,7 +2,7 @@ import {RequestHandler} from "express";
 let axios = require("axios")
 
 export const relayAPIRequest:RequestHandler = ((req,res, next) => {
-    if (req.body.url == /(https?:\/\/)re\.jrc\.ec\.europa\.eu.+/)
+    if (req.body.url == /((https?:\/\/)re\.jrc\.ec\.europa\.eu.+)|((https?:\/\/)nominatim\.openstreetmap\.org.+)/) res.send(403)
     if(req.body.method == "GET"){
         axios.get(req.body.url)
             .then((result:any) => res.json(result.data))
