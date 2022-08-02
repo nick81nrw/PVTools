8b55e14b5720be38dcfeb63b7dd70b281419d058
Nick81nrw <nick@mybergmann.net>
2022-08-02 08:44:09 +0200

    add youtube video

--- pvtools/pages/index.vue
+++ pvtools/pages/index.vue
@@ -4,6 +4,9 @@
       <b-col>
         <h1>PVTools</h1>
       </b-col>
+      <b-col>
+        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" width="200" height="100" type="text/html" src="https://www.youtube.com/embed/FKSDynkXchY?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0"></iframe>
+      </b-col>
       <b-col
         align-v="baseline"
         cols="auto" 
