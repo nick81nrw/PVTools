3bbc4cfbfe9522908940680e87fbe67c259fc290
Nick81nrw <nick@mybergmann.net>
2022-08-02 08:45:33 +0200

    send http code 403 on relay

--- backend/api/relay.ts
+++ backend/api/relay.ts
@@ -2,11 +2,13 @@ import {RequestHandler} from "express";
 let axios = require("axios")
 
 export const relayAPIRequest:RequestHandler = ((req,res, next) => {
-    if (req.body.url == /((https?:\/\/)re\.jrc\.ec\.europa\.eu.+)|((https?:\/\/)nominatim\.openstreetmap\.org.+)/) res.send(403)
+    if (req.body.url == /((https?:\/\/)re\.jrc\.ec\.europa\.eu.+)|((https?:\/\/)nominatim\.openstreetmap\.org.+)/) return res.send(403)
     if(req.body.method == "GET"){
         axios.get(req.body.url)
             .then((result:any) => res.json(result.data))
             .catch((error:any) => console.error(error))
+    } else {
+        return res.send(403)
     }
 
 
