7c986e826043f352dd1ba2a7ce6e8f53b38fc1d1
Stefan Mayr <stefan@mayr-stefan.de>
2023-12-17 00:18:37 +0100

    Fix consumption tests
    - call convertConsumptionCSV with year matching the test CSV file
    - reenable test for createCsv with correct filesize and add line count
      to the check

--- pvtools/functions/convertConsumpionUploads.test.js
+++ pvtools/functions/convertConsumpionUploads.test.js
@@ -9,7 +9,7 @@ const csvData = fs.readFileSync(__dirname + '/ImportTest.csv','utf8')
 
 describe('read csv file', ()=> {
     test('result has entries', ()=>{
-        const result = convertConsumptionCSV(csvData)
+        const result = convertConsumptionCSV(csvData, 2023)
         console.log(result)
         expect(typeof result == 'object' ).toBe(true)
     })
