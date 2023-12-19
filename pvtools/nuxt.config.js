da13cd6f13c7e8af2779c0433d6e2c99d0cd4301
Nick81nrw <nick@mybergmann.net>
2023-12-19 14:19:55 +0100

    add docker compose + proxy

--- pvtools/nuxt.config.js
+++ pvtools/nuxt.config.js
@@ -42,7 +42,7 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL:
       process.env.NODE_ENV == 'production'
-        ? `https://${process.env.APP_URL}`
+        ? `${process.env.APP_URL}`
         : 'http://localhost:8082',
     //  baseURL: 'http://localhost:8082',
   },
