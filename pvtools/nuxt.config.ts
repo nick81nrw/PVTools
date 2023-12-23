cb7c97f412aee373c3e3f32421094b8b0fdea9bc
albertlast <mathiaspapealbert@hotmail.com>
2023-12-23 09:44:43 +0100

    gtg

--- pvtools/nuxt.config.ts
+++ pvtools/nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  modules: ['nuxt-quasar-ui', '@nuxt/test-utils/module'],
+  modules: ['nuxt-quasar-ui', '@nuxt/test-utils/module', 'nuxt-gtag'],
+  app: {
+    head: {
+      title: 'PVTool @AkkuDoktor - Berechne die Größe deines Speichers selbst',
+      htmlAttrs: {
+        lang: 'de',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  gtag: {
+    id: 'G-XXXXXXXXXX',
+    initialConsent: false,
+  },
   devtools: { enabled: true },
 })
